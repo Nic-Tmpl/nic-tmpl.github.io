195531cf76c0027425d5183db94978ba3375930e
Nic-Temple <nictemplepiano@gmail.com>
2023-01-12 12:04:26 -0800

    changes to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { LightThemeScreen } from './screens/LightThemeScreen';
 import { DarkThemeScreen } from './screens/DarkThemeScreen';
@@ -7,12 +7,12 @@ import { DarkThemeScreen } from './screens/DarkThemeScreen';
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/alternate" element={<DarkThemeScreen />} />
         <Route path="/" element={<LightThemeScreen />} />
       </Routes>
-  </BrowserRouter>
+  </HashRouter>
   );
 }
 
